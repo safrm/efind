6e62c7e8ee0b534beca58e1afbd4ce8021963838
Miroslav Safr <Miroslav.Safr@gmail.com>
2013-05-29 12:35:22 +0300

    New: javafind - case sensitive search, filename search, color results, added testing files
    Signed-off-by: Miroslav Safr <Miroslav.Safr@tieto.com>

---
+++ test/javafind-hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	/**
+	 * @param args string2find
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("String2find");
+	}
+
+}
