7309ceef240f8b92c2137d7895c33b5503cb7fa8
Miroslav Safr <Miroslav.Safr@gmail.com>
2013-05-30 09:47:33 +0300

    New: shfind - case sensitive search, filename search, color results, added testing files, disable content search flag
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

---
+++ test/shfind-script with whitespaces.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#extended recursive search for documents - sh script files (sh, sh scripts without extension)
+#web: http://safrm.net/projects/efind
+#author: Miroslav Safr <miroslav.safr@gmail.com> 
+echo "shfind test string2find"
+echo "shfind test String2Find"
+
