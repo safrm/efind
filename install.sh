3fc53a2a94265f97f543c6493a7366af1de54c4f
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-05-27 22:24:28 +0300

    New: install supports msys
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@
 BINDIR=/usr/bin
 COMPLETION_DIR=/etc/bash_completion.d
 MANDIR=/usr/share/man
-
 #root check
-USERID=`id -u`
-[ $USERID -eq "0" ] || {
+[ `id -u` -ne 0 ] && [ `uname -s | grep -i linux` ] && {
     echo "I cannot continue, you should be root or run it with sudo!"
     exit 0
 }
+if command -v install 1>/dev/null 2>&1 ; then
+    INSTALL_755="install -m 0755 -v " ; INSTALL_644="install -m 0644 -v "
+else
+    INSTALL_755="cp -afv " ; INSTALL_644="cp -afv "
+fi
 
 #automatic version
 if command -v appver 1>/dev/null 2>&1 ; then . appver ; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'` ; fi
@@ -21,52 +24,52 @@ if command -v appver 1>/dev/null 2>&1 ; then . appver ; else APP_SHORT_VERSION=N
 jss-docs-update ./doc
 
 mkdir -p -m 0755 $BINDIR
-install -m 0777 -v ./efind  $BINDIR/
+$INSTALL_755 ./efind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/efind && rm -f $BINDIR/efind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/efind && rm -f $BINDIR/efind.bkp
-install -m 0777 -v ./cppfind  $BINDIR/
+$INSTALL_755 ./cppfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/cppfind && rm -f $BINDIR/cppfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/cppfind && rm -f $BINDIR/cppfind.bkp
-install -m 0777 -v ./docfind  $BINDIR/
+$INSTALL_755 ./docfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/docfind && rm -f $BINDIR/docfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/docfind && rm -f $BINDIR/docfind.bkp
-install -m 0777 -v ./gcfind  $BINDIR/
+$INSTALL_755 ./gcfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gcfind && rm -f $BINDIR/gcfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gcfind && rm -f $BINDIR/gcfind.bkp
-install -m 0777 -v ./htmlfind  $BINDIR/
+$INSTALL_755 ./htmlfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/htmlfind && rm -f $BINDIR/htmlfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/htmlfind && rm -f $BINDIR/htmlfind.bkp
-install -m 0777 -v ./javafind  $BINDIR/
+$INSTALL_755 ./javafind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/javafind && rm -f $BINDIR/javafind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/javafind && rm -f $BINDIR/javafind.bkp
-install -m 0777 -v ./pdffind  $BINDIR/
+$INSTALL_755 ./pdffind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/pdffind && rm -f $BINDIR/pdffind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/pdffind && rm -f $BINDIR/pdffind.bkp
-install -m 0777 -v ./pptfind  $BINDIR/
+$INSTALL_755 ./pptfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/pptfind && rm -f $BINDIR/pptfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/pptfind && rm -f $BINDIR/pptfind.bkp
-install -m 0777 -v ./shfind  $BINDIR/
+$INSTALL_755 ./shfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/shfind && rm -f $BINDIR/shfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/shfind && rm -f $BINDIR/shfind.bkp
-install -m 0777 -v ./specfind  $BINDIR/
+$INSTALL_755 ./specfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/specfind && rm -f $BINDIR/specfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/specfind && rm -f $BINDIR/specfind.bkp
-install -m 0777 -v ./txtfind  $BINDIR/
+$INSTALL_755 ./txtfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/txtfind && rm -f $BINDIR/txtfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/txtfind && rm -f $BINDIR/txtfind.bkp
-install -m 0777 -v ./todofind  $BINDIR/
+$INSTALL_755 ./todofind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/todofind && rm -f $BINDIR/todofind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/todofind && rm -f $BINDIR/todofind.bkp
-install -m 0777 -v ./xmlfind  $BINDIR/
+$INSTALL_755 ./xmlfind  $BINDIR/
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/xmlfind && rm -f $BINDIR/xmlfind.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/xmlfind && rm -f $BINDIR/xmlfind.bkp
 
-install -m 0777 -v ./pyfind  $BINDIR/
+$INSTALL_755 ./pyfind  $BINDIR/
 
 mkdir -p -m 0755 $COMPLETION_DIR
-install -m 0777 -v ./efind_completion  $COMPLETION_DIR/
+$INSTALL_755 ./efind_completion  $COMPLETION_DIR/
 
 MANPAGES=`find ./doc/manpages -type f`
-install -d -m 755 $MANDIR/man1
-install -m 644 $MANPAGES $MANDIR/man1
+mkdir -p -m 755 $MANDIR/man1
+$INSTALL_644 $MANPAGES $MANDIR/man1
 
