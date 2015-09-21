ab276acc8ef237dee90a63d7f51066a7f7411823
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-09-21 21:44:39 +0300

    appver-installer 1.3.0
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -6,40 +6,36 @@
 #own tests
 ./efind-test.sh -ld
 
-mkdir -p -m 0755 $BINDIR
+$MKDIR_755 $BINDIR
 $INSTALL_755 ./efind $BINDIR
-update_version_and_date $BINDIR/efind
+appver_update_version_and_date $BINDIR/efind
 $INSTALL_755 ./cppfind $BINDIR
-update_version_and_date $BINDIR/cppfind
+appver_update_version_and_date $BINDIR/cppfind
 $INSTALL_755 ./docfind $BINDIR
-update_version_and_date $BINDIR/docfind
+appver_update_version_and_date $BINDIR/docfind
 $INSTALL_755 ./gcfind $BINDIR
-update_version_and_date $BINDIR/gcfind
+appver_update_version_and_date $BINDIR/gcfind
 $INSTALL_755 ./htmlfind $BINDIR
-update_version_and_date $BINDIR/htmlfind
+appver_update_version_and_date $BINDIR/htmlfind
 $INSTALL_755 ./javafind $BINDIR
-update_version_and_date $BINDIR/javafind
+appver_update_version_and_date $BINDIR/javafind
 $INSTALL_755 ./pdffind $BINDIR
-update_version_and_date $BINDIR/pdffind
+appver_update_version_and_date $BINDIR/pdffind
 $INSTALL_755 ./pptfind $BINDIR
-update_version_and_date $BINDIR/pptfind
+appver_update_version_and_date $BINDIR/pptfind
 $INSTALL_755 ./shfind $BINDIR
-update_version_and_date $BINDIR/shfind
+appver_update_version_and_date $BINDIR/shfind
 $INSTALL_755 ./specfind $BINDIR
-update_version_and_date $BINDIR/specfind
+appver_update_version_and_date $BINDIR/specfind
 $INSTALL_755 ./txtfind $BINDIR
-update_version_and_date $BINDIR/txtfind
+appver_update_version_and_date $BINDIR/txtfind
 $INSTALL_755 ./todofind $BINDIR
-update_version_and_date $BINDIR/todofind
+appver_update_version_and_date $BINDIR/todofind
 $INSTALL_755 ./xmlfind $BINDIR
-update_version_and_date $BINDIR/xmlfind
+appver_update_version_and_date $BINDIR/xmlfind
 $INSTALL_755 ./pyfind $BINDIR
-update_version_and_date $BINDIR/pyfind
+appver_update_version_and_date $BINDIR/pyfind
 
-mkdir -p -m 0755 $COMPLETION_DIR
+$MKDIR_755 $COMPLETION_DIR
 $INSTALL_755 ./efind_completion  $COMPLETION_DIR
-
-MANPAGES=`find ./doc/manpages -type f`
-mkdir -p -m 755 $MANDIR/man1
-$INSTALL_644 $MANPAGES $MANDIR/man1
 
